ef3ced78e15b7cec8f9adea5168713f545ed0b28
Pierre-André DEWITTE <pierre-andre@seeyoucloud.com>
2019-09-30 14:01:17 +0200

    Ajout import api

--- technical-training-13.0-02-fields/openacademy/models/course.py
+++ technical-training-13.0-02-fields/openacademy/models/course.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Course(models.Model):
--- technical-training-13.0-02-fields/openacademy/models/partner.py
+++ technical-training-13.0-02-fields/openacademy/models/partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-, api
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Partner(models.Model):
