05001d25e40f54699e8728384cda2031247ee2d3
Pierre-André DEWITTE <pierre-andre@seeyoucloud.com>
2019-09-30 13:56:25 +0200

    Tentative comptage + statut

--- technical-training-13.0-02-fields/openacademy/models/course.py
+++ technical-training-13.0-02-fields/openacademy/models/course.py
@@ -22,10 +22,27 @@ class Session(models.Model):
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
     state = fields.Selection([('draft', "Draft"), ('confirmed', "Confirmed"), ('done', "Done")], default='draft')
+    attendance_state = fields.Selection([('ok', "OK"), ('under', "Sous-remplie"), ('over', "Over-bookée")], default='ok')
 
     start_date = fields.Date(default=fields.Date.context_today)
     duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
+    maximum_capacity = fields.Int(string="Capacité d'accueil", default=10)
+    total_attendees = fields.Float(compute='_compute_attendies', store=True)
 
     instructor_id = fields.Many2one('openacademy.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('openacademy.partner', string="Attendees")
+
+@api.depends('attendee_ids')
+def _compute_attendies(self):
+    for record in self:
+        record.total_attendees = len(record.attendee_ids)
+
+@api.onchange('total_attendees')
+def check_capacity(self):
+    if (self.maximum_capacity > len(record.attendee_ids)):
+        self.attendance_state = 'over'
+    if (self.maximum_capacity < len(record.attendee_ids)):
+        self.attendance_state = 'under'
+    if (self.maximum_capacity == len(record.attendee_ids)):
+        self.attendance_state = 'ok'
