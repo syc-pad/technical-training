c97bdbda2f731fef472fc9b33f399f2ef4d4c3fd
Pierre-André DEWITTE <pierre-andre@seeyoucloud.com>
2019-09-30 10:00:43 +0200

    Ajout du nom et du prénom de l'attendee

--- technical-training-13.0-01-models/openacademy/models/attendee.py
+++ technical-training-13.0-01-models/openacademy/models/attendee.py
@@ -6,3 +6,8 @@ class Attendee(models.Model):
     _name = 'attendee'
     _description = 'Attendee member of the Academy'
     _order = 'id desc'
+
+    firstname = fields.Char(required=True)
+    lastname = fields.Char(required=True)
+
+    # Class (pour savoir dans quelle classe il est inscrit)
