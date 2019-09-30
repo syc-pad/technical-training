b91363d12e08e23d0fe31ab9309f2f092e753b70
Pierre-André DEWITTE <pierre-andre@seeyoucloud.com>
2019-09-30 10:32:43 +0200

    Ajout du niveau de difficulté du cours, du nom/prénom du maester, et du nom du cours

--- technical-training-13.0-01-models/openacademy/models/academyclass.py
+++ technical-training-13.0-01-models/openacademy/models/academyclass.py
@@ -1,8 +1,17 @@
 import datetime
 from dateutil import relativedelta
 from odoo import api, fields, models, _
+ACADEMYCLASS_LEVEL = [
+    ('0', 'All'),
+    ('1', 'Beginner'),
+    ('2', 'Intermediate'),
+    ('3', 'Expert'),
+]
 
 class Academyclass(models.Model):
     _name = 'academyclass'
     _description = 'Class created by a Maester'
     _order = 'id desc'
+
+    coursename = fields.Char(required=True)
+    level = fields.Selection(ACADEMYCLASS_LEVEL, string='Level', default='0')
--- technical-training-13.0-01-models/openacademy/models/maester.py
+++ technical-training-13.0-01-models/openacademy/models/maester.py
@@ -6,3 +6,7 @@ class Maester(models.Model):
     _name = 'maester'
     _description = 'Maester giving courses in the Academy'
     _order = 'id desc'
+
+    firstname = fields.Char(required=True)
+    lastname = fields.Char(required=True)
+    
